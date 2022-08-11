8864de00d2c2e49b9bc8f204985f0fae4e422f5e
devhiko <aydin96tr@gmail.com>
2022-08-11 23:46:07 +0300

    added todo and edited comps

--- pages/rehber/index.tsx
+++ pages/rehber/index.tsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import styles from './index.module.css'
 
 const Rehber = () => {
+  // todo: add graphql api 
+  const kisiler = ['ahmet', 'veli', 'hüseyin']
+
+  const kisi = () => kisiler.map(kisi => <div className={styles.kisi} key={kisi}>{kisi}</div>)
+
   return (
-    <div>Rehber</div>
+    <div>
+      {kisi()}
+    </div>
   )
 }
 
