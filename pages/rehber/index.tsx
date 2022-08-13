0ade491b4121a5d6324f71280af44de2f65ccef3
devhiko <aydin96tr@gmail.com>
2022-08-14 00:26:37 +0300

    sorted content

--- pages/rehber/index.tsx
+++ pages/rehber/index.tsx
@@ -17,7 +17,7 @@ type RehberProps = {
 }
 
 export const getStaticProps = async () => {
-  const res: EntryCollection<EntryFields> = await client.getEntries({ 'content_type': 'kisi' })
+  const res: EntryCollection<EntryFields> = await client.getEntries({ order: 'fields.adsoyad', 'content_type': 'kisi' })
 
   return {
     props: {
@@ -47,6 +47,7 @@ const Rehber = ({ kisiler }: RehberProps) => {
 
   return (
     <div className={styles.container}>
+      {/* // todo: ADD SEARCH FUNCTION */}
       <Form className={styles.form}>
         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
           <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
