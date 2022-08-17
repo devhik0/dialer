3f388025fd04d347cdb372d0ff6880a4f52b237f
devhiko <aydin96tr@gmail.com>
2022-08-17 16:25:49 +0300

    added prettier

--- components/layout/Pads.tsx
+++ components/layout/Pads.tsx
@@ -1,74 +1,99 @@
-import { Entry } from 'contentful'
-import Image from "next/image"
-import { ChangeEvent, MouseEvent, useState } from 'react'
-import { Button, Container, Form, Offcanvas, OffcanvasProps, Row } from 'react-bootstrap'
-import { EntryFields } from '../../pages/rehber/index'
-import CallCanvas from './CallCanvas'
-import Pad from './Pad'
-import styles from './Pads.module.css'
+import { Entry } from "contentful";
+import Image from "next/image";
+import { ChangeEvent, MouseEvent, useState } from "react";
+import {
+  Button,
+  Container,
+  Form,
+  Offcanvas,
+  OffcanvasProps,
+  Row,
+} from "react-bootstrap";
+import { EntryFields } from "../../pages/rehber/index";
+import CallCanvas from "./CallCanvas";
+import Pad from "./Pad";
+import styles from "./Pads.module.css";
 
 type PadsProps = OffcanvasProps & {
-  kisiler: Entry<EntryFields>[]
-}
+  kisiler: Entry<EntryFields>[];
+};
 
 // * Floating pads button with offcanvas
 const Pads = ({ name, kisiler, ...props }: PadsProps) => {
   // dial offcanvas state i
-  const [show, setShow] = useState(false)
-  const handleShow = () => setShow(true)
-  const handleClose = () => setShow(false)
+  const [show, setShow] = useState(false);
+  const handleShow = () => setShow(true);
+  const handleClose = () => setShow(false);
 
   // search state i
-  const [query, SetQuery] = useState('')
+  // const [query, SetQuery] = useState('')
 
   // input ve pad state i
-  const [val, setVal] = useState('')
+  const [val, setVal] = useState("");
 
-  // events 
+  // events
   const handleClick = (evt: MouseEvent<HTMLButtonElement>) => {
-    setVal(val.concat(evt.currentTarget.value))
-    SetQuery(val.concat(evt.currentTarget.value))
-  }
+    // setVal(val.concat(evt.currentTarget.value))
+    // SetQuery(val.concat(evt.currentTarget.value))
+  };
 
   const handleChange = (evt: ChangeEvent<HTMLInputElement>) => {
-    setVal(evt.target.value)
-  }
+    // setVal(evt.target.value)
+  };
 
   // silme işlevi
   const handleDelete = () => {
-    const res = Array.from(val)
-    res.pop()
-    setVal(res.join(''))
-  }
+    // console.log(query)
+    // const res = Array.from(val)
+    // res.pop()
+    // setVal(res.join(''))
+  };
 
+  // console.log(query)
   // ! error: silince eski sonucları göstermiyor !
   // arama sonucu
-  const sonuc = kisiler
-    .map(kisi => kisi.fields.tel)
-    .filter(k => {
-      console.log(query)
-      return k.includes(query)
-    })
-    .map(res => query && <li className={styles['search-result']} key={res}>{res}</li>)
+  // const sonuc = kisiler
+  //   .map(kisi => kisi.fields.tel)
+  //   .filter(k => k.includes(query))
+  //   .map(res => query && <li className={styles['search-result']} key={res}>{res}</li>)
 
-  // pads 
-  const buttons = [1, 2, 3, 4, 5, 6, 7, 8, 9, '*', 0, '#']
+  // pads
+  const buttons = [1, 2, 3, 4, 5, 6, 7, 8, 9, "*", 0, "#"];
 
   return (
     <>
       <Button onClick={handleShow} className={styles.fab}>
-        <Image src='/dialpad.svg' alt='dialpad' width={32} height={32} />
+        <Image src="/dialpad.svg" alt="dialpad" width={32} height={32} />
       </Button>
       {/* // * Dialpad Offcanvas */}
-      <Offcanvas style={{ height: '100vh' }} show={show} onHide={handleClose} {...props}>
-        <div data-testid='kb' className={styles['search-results']}>
-          {sonuc.length !== 0 ? <>Sonuçlar: {sonuc}</> : ''}
+      <Offcanvas
+        style={{ height: "100vh" }}
+        show={show}
+        onHide={handleClose}
+        {...props}
+      >
+        <div data-testid="kb" className={styles["search-results"]}>
+          {/* {sonuc.length !== 0 ? <>Sonuçlar: {sonuc}</> : ''} */}
         </div>
         <Offcanvas.Header className={styles.offheader} closeButton>
-          <Form.Control className={styles.offinput} value={val} onChange={handleChange} />
-          <Button variant='outline-muted' style={{ padding: '.75rem' }} onClick={handleDelete}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor"
-              className="bi bi-backspace" viewBox="0 0 16 16">
+          <Form.Control
+            className={styles.offinput}
+            value={val}
+            onChange={handleChange}
+          />
+          <Button
+            variant="outline-muted"
+            style={{ padding: ".75rem" }}
+            onClick={handleDelete}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="32"
+              height="32"
+              fill="currentColor"
+              className="bi bi-backspace"
+              viewBox="0 0 16 16"
+            >
               <path d="M5.83 5.146a.5.5 0 0 0 0 .708L7.975 8l-2.147 2.146a.5.5 0 0 0 .707.708l2.147-2.147 2.146 2.147a.5.5 0 0 0 .707-.708L9.39 8l2.146-2.146a.5.5 0 0 0-.707-.708L8.683 7.293 6.536 5.146a.5.5 0 0 0-.707 0z" />
               <path d="M13.683 1a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-7.08a2 2 0 0 1-1.519-.698L.241 8.65a1 1 0 0 1 0-1.302L5.084 1.7A2 2 0 0 1 6.603 1h7.08zm-7.08 1a1 1 0 0 0-.76.35L1 8l4.844 5.65a1 1 0 0 0 .759.35h7.08a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1h-7.08z" />
             </svg>
@@ -76,16 +101,48 @@ const Pads = ({ name, kisiler, ...props }: PadsProps) => {
         </Offcanvas.Header>
         <Offcanvas.Body>
           <Container fluid className={styles.pads}>
-            <Row><Pad handleClick={handleClick} btns={buttons} firstIndex={0} lastIndex={3} /></Row>
-            <Row><Pad handleClick={handleClick} btns={buttons} firstIndex={3} lastIndex={6} /></Row>
-            <Row><Pad handleClick={handleClick} btns={buttons} firstIndex={6} lastIndex={9} /></Row>
-            <Row><Pad handleClick={handleClick} btns={buttons} firstIndex={9} lastIndex={12} /></Row>
-            <CallCanvas kisiler={kisiler} name={'bottom'} placement={'bottom'} />
+            <Row>
+              <Pad
+                handleClick={handleClick}
+                btns={buttons}
+                firstIndex={0}
+                lastIndex={3}
+              />
+            </Row>
+            <Row>
+              <Pad
+                handleClick={handleClick}
+                btns={buttons}
+                firstIndex={3}
+                lastIndex={6}
+              />
+            </Row>
+            <Row>
+              <Pad
+                handleClick={handleClick}
+                btns={buttons}
+                firstIndex={6}
+                lastIndex={9}
+              />
+            </Row>
+            <Row>
+              <Pad
+                handleClick={handleClick}
+                btns={buttons}
+                firstIndex={9}
+                lastIndex={12}
+              />
+            </Row>
+            <CallCanvas
+              kisiler={kisiler}
+              name={"bottom"}
+              placement={"bottom"}
+            />
           </Container>
         </Offcanvas.Body>
       </Offcanvas>
     </>
-  )
-}
+  );
+};
 
-export default Pads
+export default Pads;
--- components/layout/Search.tsx
+++ components/layout/Search.tsx
@@ -1,40 +1,55 @@
-import styles from './Search.module.css'
-import { Form } from 'react-bootstrap'
-import { ChangeEvent, useState } from 'react'
-import { Entry } from 'contentful'
-import { EntryFields } from '../../pages/rehber/index'
+import styles from "./Search.module.css";
+import { Form } from "react-bootstrap";
+import { ChangeEvent, useState } from "react";
+import { Entry } from "contentful";
+import { EntryFields } from "../../pages/rehber/index";
 
 type SearchProps = {
-  kisiler: Entry<EntryFields>[]
-}
+  kisiler: Entry<EntryFields>[];
+};
+
+// todo: bunu düzenle
 
 const Search = ({ kisiler }: SearchProps) => {
   // search state i
-  const [query, SetQuery] = useState('')
+  // const [query, SetQuery] = useState('')
 
   const handleChange = (evt: ChangeEvent<HTMLInputElement>) => {
-    SetQuery(evt.target.value.toLowerCase())
-  }
+    // SetQuery(evt.target.value.toLowerCase())
+  };
 
-  const sonuc = kisiler
-    .map(kisi => kisi.fields.adsoyad)
-    .filter(k => k.toLowerCase().includes(query))
-    .map(res => query && <li className={styles['search-result']} key={res}>{res}</li>)
+  // const sonuc = kisiler
+  //   .map(kisi => kisi.fields.adsoyad)
+  //   .filter(k => k.toLowerCase().includes(query))
+  //   .map(res => query && <li className={styles['search-result']} key={res}>{res}</li>)
 
   return (
     <div className={styles.search}>
       <Form className={styles.form}>
-        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="20"
+          height="20"
+          fill="currentColor"
+          className="bi bi-search"
+          viewBox="0 0 16 16"
+        >
           <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
         </svg>
-        <Form.Control onChange={handleChange} value={query} type="search" placeholder="Kişi arayın" aria-label="Search" />
+        <Form.Control
+          onChange={handleChange}
+          // value={query}
+          type="search"
+          placeholder="Kişi arayın"
+          aria-label="Search"
+        />
       </Form>
-      <div data-testid='kb' className={styles['search-results']}>
+      <div data-testid="kb" className={styles["search-results"]}>
         <b>Sonuçlar</b>
-        {sonuc.length !== 0 ? sonuc : <p>Böyle biri yok</p>}
+        {/* {sonuc.length !== 0 ? sonuc : <p>Böyle biri yok</p>} */}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
