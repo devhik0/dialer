4f54e80fc0cd7bd36a5a4999a7ca1cf2628e9573
devhiko <aydin96tr@gmail.com>
2022-09-01 16:55:56 +0300

    try pcss with sugges.

--- postcss.config.js
+++ postcss.config.js
@@ -16,9 +16,13 @@ module.exports = {
     [
       "@fullhuman/postcss-purgecss",
       {
-        content: ["./pages/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+        content: [
+          "./pages/**/*.{js,jsx,ts,tsx}",
+          "./components/**/*.{js,jsx,ts,tsx}",
+          "./node_modules/react-bootstrap/**/*",
+        ],
         defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-        safelist: ["html", "body", "./styles/**/*.{css, scss}"],
+        safelist: ["html", "body"],
       },
     ],
   ],
