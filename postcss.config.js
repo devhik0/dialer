13d782113c40732c2c3f9afd84ba25cb9481629e
devhiko <aydin96tr@gmail.com>
2022-09-01 17:09:53 +0300

    back to original

--- postcss.config.js
+++ postcss.config.js
@@ -16,13 +16,9 @@ module.exports = {
     [
       "@fullhuman/postcss-purgecss",
       {
-        content: [
-          "./pages/**/*.{js,jsx,ts,tsx}",
-          "./components/**/*.{js,jsx,ts,tsx}",
-          "./node_modules/react-bootstrap/**/*",
-        ],
+        content: ["./pages/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
         defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-        safelist: ["html", "body", "./node_modules/react-bootstrap/"],
+        safelist: ["html", "body"],
       },
     ],
   ],
