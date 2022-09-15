8a25b4cb4da14cc33a49289d9851e727cc40f0ab
devhiko <aydin96tr@gmail.com>
2022-09-15 18:32:33 +0300

    added optimistic cache updates

--- features/api/apiSlice.ts
+++ features/api/apiSlice.ts
@@ -18,9 +18,28 @@ const C_BASE_URL = `https://cdn.contentful.com/spaces/${space}/environments/mast
 export const kisilerApi = createApi({
   reducerPath: "kisilerApi",
   baseQuery: fetchBaseQuery({ baseUrl: C_BASE_URL }),
+  tagTypes: ["Kisi"],
+  keepUnusedDataFor: 30,
   endpoints: (builder) => ({
     kisileriGetir: builder.query<Response, string>({
-      query: () => `entries?access_token=${acc_token}&content_type=${content_type}`,
+      query: () => `entries?access_token=${acc_token}&content_type=${content_type}&order=fields.adsoyad`,
+      onQueryStarted(kisiler, { dispatch, queryFulfilled }) {
+        const patchResult = dispatch(
+          kisilerApi.util.updateQueryData("kisileriGetir", "kisiler", (draft) => {
+            Object.assign(draft, kisiler);
+          })
+        );
+        queryFulfilled.catch(patchResult.undo);
+      },
+
+      providesTags: (result) => {
+        if (result) {
+          return [...result.items.map(({ sys }) => ({ type: "Kisi" as const, sys })), "Kisi"];
+        } else {
+          return ["Kisi"];
+        }
+      },
+      keepUnusedDataFor: 10,
     }),
   }),
 });
