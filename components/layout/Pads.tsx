a05485a3d5f5afe355ac8e2dee0b011867b6729e
hikmet <aydin96tr@gmail.com>
2023-01-14 17:41:39 +0300

    svg -> i

--- components/layout/Pads.tsx
+++ components/layout/Pads.tsx
@@ -60,19 +60,7 @@ const Pads = ({ ...props }: OffcanvasProps) => {
       {/* Dialpad Offcanvas */}
       <Offcanvas style={{ height: "100vh" }} show={show} onHide={handleClose} {...props}>
         <Button onClick={handleClose} variant="outline" className={styles.back}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="20"
-            height="20"
-            fill="currentColor"
-            className="bi bi-arrow-left"
-            viewBox="0 0 16 16"
-          >
-            <path
-              fillRule="evenodd"
-              d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"
-            />
-          </svg>
+          <i className="bi bi-arrow-left"></i>
         </Button>
 
         <div data-testid="kb" className={styles["search-results"]}>
@@ -84,17 +72,7 @@ const Pads = ({ ...props }: OffcanvasProps) => {
         <Offcanvas.Header className={styles.offheader}>
           <Form.Control className={styles.offinput} onChange={handleChange} value={val} type="search" />
           <Button variant="outline" style={{ marginRight: ".5rem", padding: ".75rem .5rem" }} onClick={handleDelete}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="32"
-              height="32"
-              fill="currentColor"
-              className="bi bi-backspace"
-              viewBox="0 0 16 16"
-            >
-              <path d="M5.83 5.146a.5.5 0 0 0 0 .708L7.975 8l-2.147 2.146a.5.5 0 0 0 .707.708l2.147-2.147 2.146 2.147a.5.5 0 0 0 .707-.708L9.39 8l2.146-2.146a.5.5 0 0 0-.707-.708L8.683 7.293 6.536 5.146a.5.5 0 0 0-.707 0z" />
-              <path d="M13.683 1a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-7.08a2 2 0 0 1-1.519-.698L.241 8.65a1 1 0 0 1 0-1.302L5.084 1.7A2 2 0 0 1 6.603 1h7.08zm-7.08 1a1 1 0 0 0-.76.35L1 8l4.844 5.65a1 1 0 0 0 .759.35h7.08a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1h-7.08z" />
-            </svg>
+            <i className="bi bi-backspace"></i>
           </Button>
         </Offcanvas.Header>
 
