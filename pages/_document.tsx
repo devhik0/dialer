5c760d8c1baf06769eb60a5279673733aa859b68
hikmet <aydin96tr@gmail.com>
2023-01-14 20:09:15 +0300

    e

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,10 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html>
       <Head>
-        <link
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css"
-        ></link>
+        <link rel="preload" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css"></link>
       </Head>
       <body>
         <Main />
