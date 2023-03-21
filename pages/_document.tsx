7d4650a0884b8cdde9c8fccc6ada227bf6c9a3b1
hikmet <aydin96tr@gmail.com>
2023-03-21 20:48:11 +0300

    fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,10 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link rel="preload" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css"></link>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css"
+        ></link>
       </Head>
       <body>
         <Main />
